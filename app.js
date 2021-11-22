0e0f8c7645c2b4b47936f1d58cf9a78262706e65
saykat1 <nursaykat@gmail.com>
2021-11-22 17:09:55 +0600

    create global error handler

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes')
 
+const AppError = require('./utils/appError');
+const AppErrorHandler = require('./controller/appErrorController');
+
 const app = express();
 
 if (process.env.NODE_ENV = 'development') {
@@ -20,5 +23,32 @@ app.use(express.static(`${__dirname}/public`))
 
 
 app.use('/api/v1/tours', tourRouter)
+
+
+// app.all('*', (req, res, next) => {
+//   const err = new Error('Page not found on '+ req.originalUrl);
+//   err.statusCode = 404;
+//   err.status = "fail"
+
+//   next(err);
+// });
+
+
+// app.use((err, req, res, next) => {
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || "error";
+
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message
+//   })
+// })
+
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Page not found on ${req.originalUrl}`, 404));
+});
+
+app.use(AppErrorHandler);
 
 module.exports = app;
